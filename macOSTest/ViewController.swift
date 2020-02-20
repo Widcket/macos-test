b37dedae84213c6f05554c554da2fab8bd8dff65
Rita Zerrizuela <zeta@widcket.com>
2020-02-20 20:04:12 -0300

    Initial Commit

---
+++ macOSTest/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  macOSTest
+//
+//  Created by Rita Zerrizuela on 20/02/2020.
+//  Copyright © 2020 Auth0. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
